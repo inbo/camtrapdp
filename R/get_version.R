2c7aa29c98c715a68f75682f79008bdc63028a66
Peter Desmet <peter.desmet@inbo.be>
2024-03-06 17:08:54 +0100

    Update get_version() documentation

--- R/get_version.R
+++ R/get_version.R
@@ -1,18 +1,19 @@
-#' Get Camtrap DP version
+#' Get the Camtrap DP version used by a dataset
 #'
-#' Gets the Camtrap DP version number used by a dataset.
-#' The version number (`1`, `1.0`, `1.2.3`, etc.) is extracted from
-#' `dataset$profile`, which should contain the URL of the used Camtrap DP
-#' standard.
+#' Extracts the Camtrap DP version number used by a dataset, by searching for
+#' `camtrap-dp/<version-number>` in `dataset$profile`, which is expected to
+#' contain the URL of the used Camtrap DP standard.
 #' When no match is found, `dataset$profile` is returned in its entirety.
 #'
 #' @inheritParams convert
-#' @return Camtrap DP version number or `dataset$profile`.
+#' @return Camtrap DP version number (e.g. `1.0`) or `dataset$profile`.
 #' @export
 get_version <- function(dataset) {
-  profile <- dataset$profile # E.g. https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/camtrap-dp-profile.json
-  pattern <- "camtrap-dp\\/\\d+(\\.\\d+){1,2}" # E.g. camtrap-dp/1, camtrap-dp/1.2.3
-  match <- grep(pattern, pattern)
+  profile <- dataset$profile
+  # E.g. https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/camtrap-dp-profile.json
+  pattern <- "camtrap-dp\\/\\d+(\\.\\d+){1,2}"
+  # Must be camtrap-dp/<major>.<minor><.optional_patch>
+  match <- grep(pattern, profile)
 
   if (length(match) > 0) {
     extracted_version <- regmatches(profile, regexpr(pattern, profile))
