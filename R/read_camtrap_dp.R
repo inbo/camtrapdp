7269434aba43d6841df770fd2183d192e2d363a2
Peter Desmet <peter.desmet@inbo.be>
2024-03-13 14:39:06 +0100

    Update read_camtrap_dp()
    - Check supported versions as part of this function
    - Create a camtrapdp object
    - Read data

--- R/read_camtrap_dp.R
+++ R/read_camtrap_dp.R
@@ -4,9 +4,38 @@
 #' https://camtrap-dp.tdwg.org) into memory.
 #'
 #' @param file Path or URL to a `datapackage.json` file.
-#' @return List describing a Camera Trap Data Package.
+#' @return Camera Trap Data Package object.
 #' @export
 read_camtrap_dp <- function(file) {
+  # Read datapackage.json
   package <- frictionless::read_package(file)
-  convert(package)
+
+  # Check version
+  version <- version(package)
+  supported_versions <- c("1.0")
+  if (!version %in% supported_versions) {
+    cli::cli_abort(
+      c(
+        "{.val {version}} is not a supported Camtrap DP version.",
+        "i" = "Supported version{?s}: {.val {supported_versions}}."
+      ),
+      class = "camtrapdp_error_unsupported_version"
+    )
+  }
+
+  # Create camtrapdp object
+  camtrapdp <- package
+  class(camtrapdp) <- c("camtrapdp", class(camtrapdp))
+  attr(camtrapdp, "version") <- version
+
+  # Read and attach csv data
+  camtrapdp$data$deployments <-
+    frictionless::read_resource(package, "deployments")
+  camtrapdp$data$media <-
+    frictionless::read_resource(package, "media")
+  camtrapdp$data$observations <-
+    frictionless::read_resource(package, "observations")
+
+  # Convert
+  convert(package, convert_to = "1.0")
 }
--- tests/testthat/test-read_camtrap_dp.R
+++ tests/testthat/test-read_camtrap_dp.R
@@ -1,3 +1,40 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("read_camtrap_dp() reads a Camtrap DP", {
+  skip_if_offline()
+  file <-
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
+  expect_no_error(read_camtrap_dp(file))
+
+  # TODO: add more tests on returned object
+})
+
+test_that("read_camtrap_dp() returns error on unsupported Camtrap DP version", {
+  skip_if_offline()
+
+  # Unsupported version
+  camtrap_dp_1.0_rc.1 <-
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0-rc.1/example/datapackage.json"
+  expect_error(
+    read_camtrap_dp(camtrap_dp_1.0_rc.1),
+    class = "camtrapdp_error_unsupported_version"
+  )
+  expect_error(
+    read_camtrap_dp(camtrap_dp_1.0_rc.1),
+    regexp = "\"1.0-rc.1\" is not a supported Camtrap DP version.",
+    fixed = TRUE
+  )
+
+  # Not a Camtrap DP
+  o_assen <- "https://zenodo.org/records/10053903/files/datapackage.json"
+  expect_error(
+    read_camtrap_dp(o_assen),
+    class = "camtrapdp_error_unsupported_version"
+  )
+})
+
+test_that("read_camtrap_dp() does not convert 1.0", {
+  skip_if_offline()
+  camtrap_dp_1.0 <-
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
+  x <- read_camtrap_dp(camtrap_dp_1.0)
+  expect_identical(version(x), "1.0")
 })
