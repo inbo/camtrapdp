5854a375e6a27e968c149710d08bb901110f2455
Peter Desmet <peter.desmet@inbo.be>
2024-03-13 14:39:32 +0100

    Reduce convert() function

--- R/convert.R
+++ R/convert.R
@@ -1,53 +1,28 @@
 #' Convert a Camera Trap Data Package
 #'
-#' Converts a Camera Trap Data Package that uses an older version of Camtrap DP
-#' to the latest version (currently `1.0`).
+#' Converts a Camera Trap Data Package object that uses an older version of
+#' the Camtrap DP standard to a newer version (currently `1.0`).
 #'
-#' @param package Camera Trap Data Package, as returned by
-#'   `frictionless::read_package()`.
-#' @return A `camtrapdp` object.
+#' @inheritParams version
+#' @param convert_to Version to convert to.
+#' @return Converted Camera Trap Data Package object.
 #' @noRd
-convert <- function(package) {
-  # Check Camtrap DP version
-  version <- get_version(package)
-  supported_versions <- c("1.0")
-  if (!version %in% supported_versions) {
-    cli::cli_abort(
-      c(
-        "{.val {version}} is not a supported Camtrap DP version.",
-        "i" = "Supported version{?s}: {.val {supported_versions}}."
-      ),
-      class = "camtrapdp_error_unsupported_version"
+convert <- function(camtrapdp, convert_to = "1.0") {
+  # Convert until the version number matches the expected version
+  while(version(camtrapdp) != convert_to) {
+    camtrapdp <- switch(
+      version(camtrapdp),
+      "0.1.6" = convert_0.1.6_to_1.0(camtrapdp),
+      "1.0" = camtrapdp
     )
   }
-
-  # Create camtrap_dp object
-  dataset <- package
-  class(dataset) <- c("camtrap_dp", class(dataset))
-  attr(dataset, "version") <- version
-
-  # TODO: Read and attach csv data (currently same for all versions)
-  # dataset$data$deployments <- frictionless::read_resource(dataset, "deployments")
-  # dataset$data$media <- frictionless::read_resource(dataset, "media")
-  # dataset$data$observations <- frictionless::read_resource(dataset, "observations")
-
-  # Convert dataset
-  while(version != "1.0") {
-    dataset <- switch(
-      version,
-      "0.1.6" = convert_0.1.6_to_1.0(dataset),
-      "1.0" = dataset
-    )
-    version <- attr(dataset, "version")
-  }
-
-  dataset
+  camtrapdp
 }
 
 #' Convert a Camtrap DP 0.1.6 to 1.0
 #' @noRd
-convert_0.1.6_to_1.0 <- function(dataset) {
+convert_0.1.6_to_1.0 <- function(camtrapdp) {
   # TODO: conversion steps for 0.1.6
-  attr(dataset, "version") <- "1.0"
-  dataset
+  attr(camtrapdp, "version") <- "1.0"
+  camtrapdp
 }
