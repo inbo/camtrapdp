226e398bd1ef567a2d40baeedab93821702f0723
Peter Desmet <peter.desmet@inbo.be>
2024-03-13 14:27:09 +0100

    Update get_version()
    - Rename to version()
    - Get attr first
    - Allow more patterns
    - Make public

--- R/version.R
+++ R/version.R
@@ -1,27 +1,41 @@
-#' Get or set the Camtrap DP version used by a package
+#' Get the used Camtrap DP version
 #'
-#' Extracts the version number used by a Camera Trap Data Package, by searching
-#' for `camtrap-dp/<version-number>` in `package$profile`, which is expected to
-#' contain the URL of the used Camtrap DP standard.
+#' Extracts the version number used by a Camera Trap Data Package.
+#' This version number indicates what version of the standard was used.
 #'
-#' @inheritParams convert
-#' @return Camtrap DP version number (e.g. `1.0`) or `package$profile` in its
-#' entirety when no version number is found.
-#' @noRd
-version <- function(package) {
-  profile <- package$profile # E.g. https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/camtrap-dp-profile.json
+#' The version number is extracted as follows:
+#' 1. The `version` attribute, if defined.
+#' 2. If undefined, a version number contained in `camtrapdp$profile`, which is
+#' expected to contain the URL to the used Camtrap DP standard.
+#' 3. `camtrapdp$profile` in its entirety (can be `NULL`).
+#'
+#' @param camtrapdp Camera Trap Data Package object, as returned by
+#'   `read_camtrap_dp()` or Frictionless Data Package, as returned by
+#'   `frictionless::read_package()`.
+#' @return Camtrap DP version number (e.g. `1.0`).
+#' @export
+version <- function(camtrapdp) {
+  # Get version from attribute
+  attr_version <- attr(camtrapdp, "version")
+  if (!is.null(attr_version)) {
+    return(attr_version)
+  }
 
-  # No profile defined
+  # Get version from profile
+  profile <- camtrapdp$profile
   if (is.null(profile)) {
     return(NA)
   }
 
-  # Find pattern "camtrap-dp/<major>.<minor><.optional_patch>"
-  pattern <- "camtrap-dp\\/\\d+(\\.\\d+){1,2}"
+  # Find pattern "camtrap-dp/<version>/" in e.g.
+  # https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/camtrap-dp-profile.json
+  pattern <- "camtrap-dp\\/([0-9A-Za-z]|\\.|-)+\\/"
   match <- grep(pattern, profile)
   if (length(match) > 0) {
     extracted_version <- regmatches(profile, regexpr(pattern, profile))
-    sub("camtrap-dp/", "", extracted_version, fixed = TRUE)
+    extracted_version <- sub("camtrap-dp/", "", extracted_version, fixed = TRUE)
+    extracted_version <- sub("/", "", extracted_version, fixed = TRUE)
+    extracted_version
   } else {
     profile
   }
