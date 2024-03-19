1e1f9b507b80d3044fb39ad4a6089580f4617fe6
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-03-19 10:31:09 +0100

    break up the long url

--- R/example_dataset.R
+++ R/example_dataset.R
@@ -9,5 +9,9 @@
 #'
 #' @examples example_dataset()
 example_dataset <- function() {
-  read_camtrap_dp(file = "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json")
+  read_camtrap_dp(file = file.path(
+    "https://raw.githubusercontent.com",
+    "tdwg",
+    "camtrap-dp", "1.0", "example", "datapackage.json"
+  ))
 }
