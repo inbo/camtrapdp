044e80e23eb3edd916346ad1f35d3230baa3ed3f
Peter Desmet <peter.desmet@inbo.be>
2024-03-19 10:59:37 +0100

    Rename camtrap_dp to camtrapdp

--- tests/testthat/test-read_camtrap_dp.R
+++ tests/testthat/test-read_camtrap_dp.R
@@ -1,24 +1,24 @@
-test_that("read_camtrap_dp() reads a Camtrap DP", {
+test_that("read_camtrapdp() reads a Camtrap DP", {
   skip_if_offline()
   file <-
     "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
-  expect_no_error(read_camtrap_dp(file))
+  expect_no_error(read_camtrapdp(file))
 
   # TODO: add more tests on returned object
 })
 
-test_that("read_camtrap_dp() returns error on unsupported Camtrap DP version", {
+test_that("read_camtrapdp() returns error on unsupported Camtrap DP version", {
   skip_if_offline()
 
   # Unsupported version
-  camtrap_dp_1.0_rc.1 <-
+  camtrapdp_1.0_rc.1 <-
     "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0-rc.1/example/datapackage.json"
   expect_error(
-    read_camtrap_dp(camtrap_dp_1.0_rc.1),
+    read_camtrapdp(camtrapdp_1.0_rc.1),
     class = "camtrapdp_error_unsupported_version"
   )
   expect_error(
-    read_camtrap_dp(camtrap_dp_1.0_rc.1),
+    read_camtrapdp(camtrapdp_1.0_rc.1),
     regexp = "\"1.0-rc.1\" is not a supported Camtrap DP version.",
     fixed = TRUE
   )
@@ -26,15 +26,15 @@ test_that("read_camtrap_dp() returns error on unsupported Camtrap DP version", {
   # Not a Camtrap DP
   o_assen <- "https://zenodo.org/records/10053903/files/datapackage.json"
   expect_error(
-    read_camtrap_dp(o_assen),
+    read_camtrapdp(o_assen),
     class = "camtrapdp_error_unsupported_version"
   )
 })
 
-test_that("read_camtrap_dp() does not convert 1.0", {
+test_that("read_camtrapdp() does not convert 1.0", {
   skip_if_offline()
-  camtrap_dp_1.0 <-
+  camtrapdp_1.0 <-
     "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
-  x <- read_camtrap_dp(camtrap_dp_1.0)
+  x <- read_camtrapdp(camtrapdp_1.0)
   expect_identical(version(x), "1.0")
 })
