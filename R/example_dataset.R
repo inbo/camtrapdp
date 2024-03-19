734aec4c780723c4f62d8332ea7047b07beb645a
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-03-19 10:23:44 +0100

    always return the most recent version, don't pass args

--- R/example_dataset.R
+++ R/example_dataset.R
@@ -1,12 +1,11 @@
 #' Create an example dataset based on the Mica data
 #'
-#' @param ...
 #' @inheritParams read_camtrap_dp
 #'
 #' @return Camera Trap Data Package object.
 #' @export
 #'
 #' @examples example_dataset()
-example_dataset <- function(...) {
-  read_camtrap_dp(file = "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json", ...)
+example_dataset <- function() {
+  read_camtrap_dp(file = "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json")
 }
