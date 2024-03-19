01912026b0d8c1927a5134a7de862a1517d8c6d3
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-03-19 12:59:55 +0100

    add tests in line with `camtraptor::get_species()`

--- tests/testthat/test-build_taxonomy.R
+++ tests/testthat/test-build_taxonomy.R
@@ -3,5 +3,18 @@ test_that("build_taxonomy() returns tibble", {
 })
 
 test_that("build_taxonomy() returns one row per species in $data$observations",{
+  number_of_species <-
+    length(unique(example_package()$data$observations$scientificName))
+})
+
+test_that("build_taxonomy() can handle missing vernacular names",{
+
+})
+
+test_that("build_taxonomy() returns NULL when there is no taxonomic information", {
+
+})
+
+test_that("build_taxonomy() fills missing values with NA when a taxonomic field is only present for some of the records", {
 
 })
