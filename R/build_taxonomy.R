99d02aa161ce70860b1ecf31e4ca0f9e83ab4157
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-03-19 13:18:15 +0100

    extract taxonomic information

---
+++ R/build_taxonomy.R
@@ -0,0 +1,14 @@
+#' Parse the taxon information from a Camera Trap Data Package object
+#'
+#' @param x
+#'
+#' @return A tibble with the taxonomic information from a Camera Trap Data
+#'   Package
+#'
+#' @examples
+build_taxonomy <- function(x) {
+  # Extract the taxonomic information only
+  taxonomic_list <- x$taxonomic
+
+
+}
