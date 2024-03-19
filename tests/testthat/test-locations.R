4c60f9ae2bf81da171d31f4f61be5c5c07a724e6
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-03-19 13:44:25 +0100

    Read example dataset via URL
    To be changed when `example_dataset()` will be created.

--- tests/testthat/test-locations.R
+++ tests/testthat/test-locations.R
@@ -1,4 +1,6 @@
 test_that("locations returns a tibble data.frame", {
+  # Read camtrap DP (to be changed when example is created)
+  dataset <- read_camtrapdp("https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json")
   locs <- locations(dataset)
   expect_true(is.data.frame(locs))
   expect_true(is_tibble(locs))
@@ -6,6 +8,8 @@ test_that("locations returns a tibble data.frame", {
 
 
 test_that("locations returns the expected columns", {
+  # Read camtrap DP (to be changed when example is created)
+  dataset <- read_camtrapdp("https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json")
   cols_locations <- c("locationID",
                  "locationName",
                  "latitude",
