b1a7ec05ba11f9a53205999b26811a80b285dd9e
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-03-20 09:50:45 +0100

    Add examples section in Roxygen doc

--- R/media.R
+++ R/media.R
@@ -6,6 +6,9 @@
 #' @return `tibble()` data frame with media.
 #' @family accessor functions
 #' @export
+#' @examples
+#' dataset <- example_dataset()
+#' media(dataset)
 media <- function(x) {
   # check_camtrapdp(x) # uncomment if check_camtrapdp() will exist
   x$data$media
--- R/observations.R
+++ R/observations.R
@@ -6,6 +6,9 @@
 #' @return `tibble()` data frame with observations.
 #' @family accessor functions
 #' @export
+#' @examples
+#' dataset <- example_dataset()
+#' observations(dataset)
 observations <- function(x) {
   # check_camtrapdp(x) # uncomment if check_camtrapdp() will exist
   x$data$observations
