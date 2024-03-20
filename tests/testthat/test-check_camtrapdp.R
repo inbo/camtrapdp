2b679d306b17f218b873c73161dc270cd4dd57ed
Peter Desmet <peter.desmet@inbo.be>
2024-03-20 16:27:18 +0100

    Rephrase

--- tests/testthat/test-check_camtrapdp.R
+++ tests/testthat/test-check_camtrapdp.R
@@ -1,4 +1,4 @@
-test_that("read_camtrapdp() returns error if not a camtrapdp object", {
+test_that("read_camtrapdp() returns error on invalid camtrapdp object", {
   skip_if_offline()
   o_assen <- "https://zenodo.org/records/10053903/files/datapackage.json"
   expect_error(
