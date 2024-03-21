451b8b5708de241ebd662209e57b8b645d938211
Peter Desmet <peter.desmet@inbo.be>
2024-03-21 14:12:33 +0100

    Update locations() documentation, add .data for all
    And use check_camtrapdp()

--- R/locations.R
+++ R/locations.R
@@ -1,27 +1,28 @@
 #' Get locations
 #'
-#' Extract unique locations of the deployments of a Camera Trap Data Package
+#' Gets the unique locations from the deployments of a Camera Trap Data Package
 #' object.
 #'
 #' @inheritParams version
-#' @return `tibble()` data frame with the following columns:
+#' @return [tibble()] data frame with the following columns:
 #' - `locationID`
 #' - `locationName`
 #' - `latitude`
 #' - `longitude`
 #' - `coordinateUncertainty`
 #' @family accessor functions
-#' @importFrom dplyr %>% .data
 #' @export
 #' @examples
-#' dataset <- example_dataset()
-#' locations(dataset)
+#' x <- example_dataset()
+#' locations(x)
 locations <- function(x) {
-  # check_camtrapdp(x) # uncomment if check_camtrapdp() will exist
+  check_camtrapdp(x)
   deployments(x) %>%
-    dplyr::distinct(.data$locationID,
-                    .data$locationName,
-                    .data$latitude,
-                    .data$longitude,
-                    .data$coordinateUncertainty)
+    dplyr::distinct(
+      .data$locationID,
+      .data$locationName,
+      .data$latitude,
+      .data$longitude,
+      .data$coordinateUncertainty
+    )
 }
