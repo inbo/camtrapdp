85c716d1bbc8b5bae20d2b103c6883f2db1c80c5
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-03-21 14:09:02 +0100

    Use dataset_no_taxon_info consequently

--- tests/testthat/test-taxonomy.R
+++ tests/testthat/test-taxonomy.R
@@ -25,7 +25,7 @@ test_that(
   "taxonomy returns scientificName column only if no taxon info in observations", {
   skip_if_offline()
   dataset_no_taxon_info  <- example_dataset()
-  dataset_no_taxon_info$data$observations <- dataset$data$observations %>%
+  dataset_no_taxon_info$data$observations <- dataset_no_taxon_info$data$observations %>%
     dplyr::select(-dplyr::starts_with("taxon."))
   expect_identical(
     taxonomy(dataset_no_taxon_info),
