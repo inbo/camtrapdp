715cee3564b0c564a39d16c0fd803de08542c11f
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-03-21 10:19:15 +0100

    Improve tests for taxonomy()

--- tests/testthat/test-taxonomy.R
+++ tests/testthat/test-taxonomy.R
@@ -1,4 +1,5 @@
 library(tibble)
+library(dplyr)
 
 test_that("taxonomy returns a tibble data.frame", {
   skip_if_offline()
@@ -8,19 +9,39 @@ test_that("taxonomy returns a tibble data.frame", {
   expect_true(is_tibble(taxa))
 })
 
-test_that("taxonomy returns the expected columns", {
+test_that("taxonomy returns the columns scientificName and with taxon prefix", {
   skip_if_offline()
   dataset <- example_dataset()
   taxa <- taxonomy(dataset)
-  cols_taxonomy <- c("scientificName")
-  expect_equal(names(taxa), cols_taxonomy)
+  expect_true("scientificName" %in% names(taxa))
+  expect_true(
+    all(
+      grepl(pattern = "^taxon\\.",
+              x = names(taxa %>% dplyr::select(-scientificName))
+            )
+    )
+  )
+})
+
+test_that(
+  "taxonomy returns scientificName column only if no taxon info in observations", {
+  skip_if_offline()
+  dataset <- example_dataset()
+  dataset$data$observations <- dataset$data$observations %>%
+    select(-starts_with("taxon."))
+  expect_identical(
+    taxonomy(dataset),
+    observations(dataset) %>%
+      distinct(scientificName) %>%
+      filter(!is.na(scientificName))
+  )
 })
 
 test_that("taxonomy returns the expected amount of rows", {
   skip_if_offline()
   dataset <- example_dataset()
   taxa <- taxonomy(dataset)
-  expect_equal(nrow(taxa),11)
+  expect_equal(nrow(taxa),10)
 })
 
 test_that("taxonomy never returns more rows than observations", {
@@ -31,12 +52,15 @@ test_that("taxonomy never returns more rows than observations", {
   expect_true(nrow(taxa) <= nrow(obs))
 })
 
-
 test_that(
   "one row returned if all observations have same taxonomy information", {
     skip_if_offline()
     dataset <- example_dataset()
     dataset$data$observations$scientificName <- "Vulpes vulpes"
+    dataset$data$observations$taxon.taxonRank <- "species"
+    dataset$data$observations$taxon.taxonID <- "https://www.checklistbank.org/dataset/COL2023/taxon/5BSG3"
+    dataset$data$observations$taxon.vernacularNames.eng <- "red fox"
+    dataset$data$observations$taxon.vernacularNames.nld <- "vos"
     taxa <- taxonomy(dataset)
     expect_equal(nrow(taxa), 1)
   })
