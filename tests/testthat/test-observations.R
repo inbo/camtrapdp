75da20601e4ac8d7d46d33be81fc54f1e408275f
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-03-21 10:18:51 +0100

    Add tests about taxonomic columns of observations

--- tests/testthat/test-observations.R
+++ tests/testthat/test-observations.R
@@ -1,3 +1,5 @@
+library(purrr)
+
 test_that("observations() returns the observations", {
   skip_if_offline()
   x <- example_dataset()
@@ -6,3 +8,19 @@ test_that("observations() returns the observations", {
     x$data$observations
   )
 })
+
+test_that("observations() returns taxonomic information if present", {
+  skip_if_offline()
+  x <- example_dataset()
+  col_taxon <- map(x$taxonomic,
+                   list_flatten,
+                   name_spec = "{outer}.{inner}") %>%
+    map(as.data.frame) %>%
+    list_rbind() %>%
+    names
+  taxon_df <- observations(x) %>%
+    select(scientificName, starts_with("taxon."))
+  names(taxon_df) <- sub('^taxon.', '', names(taxon_df))
+  expect_equal(col_taxon, names(taxon_df))
+})
+
