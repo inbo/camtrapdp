5d48cdc7bd73ff48b7c1f5ec3321147fda4b2e76
Peter Desmet <peter.desmet@inbo.be>
2024-03-22 11:50:55 +0100

    Use regex for sub

--- R/taxa.R
+++ R/taxa.R
@@ -17,5 +17,5 @@ taxa <- function(x) {
     dplyr::filter(!is.na(.data$scientificName)) %>%
     dplyr::select("scientificName", dplyr::starts_with("taxon.")) %>%
     dplyr::distinct() %>%
-    dplyr::rename_with(~ sub("taxon.", "", .x, fixed = TRUE))
+    dplyr::rename_with(~ sub("^taxon.", "", .x))
 }
