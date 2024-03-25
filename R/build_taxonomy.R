1418741845ab330074bcb86181b741176e93c4cc
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-03-25 12:31:18 +0100

    store created data.frame as object

--- R/build_taxonomy.R
+++ R/build_taxonomy.R
@@ -16,11 +16,11 @@ build_taxonomy <- function(x) {
   }
 
   # Convert list into a data.frame
-  purrr::map(
-      taxonomic_list,
-      purrr::list_flatten,
-      name_spec = "{outer}.{inner}"
-    ) %>%
+  taxonomy_df <- purrr::map(
+    taxonomic_list,
+    purrr::list_flatten,
+    name_spec = "{outer}.{inner}"
+  ) %>%
     purrr::map(as.data.frame) %>%
     purrr::list_rbind()
 }
