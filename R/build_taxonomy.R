45511b549d4439743fd3cca6541faf9e3c93e5b1
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2024-03-25 12:31:40 +0100

    use cli to create warning, base for duplicate detection

--- R/build_taxonomy.R
+++ R/build_taxonomy.R
@@ -23,4 +23,13 @@ build_taxonomy <- function(x) {
   ) %>%
     purrr::map(as.data.frame) %>%
     purrr::list_rbind()
+
+  # Warn if there are duplicate species
+  if (anyDuplicated(taxonomy_df$scientificName)) {
+    cli::cli_warn(
+      "Duplicate scientificNames present in {.arg x$taxonmic},
+      only the first one will be returned.",
+      class = "camtrapdp_warning_duplicate_scientificname"
+    )
+  }
 }
