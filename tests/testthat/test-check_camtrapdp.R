41f6b6f06b1b91a62b72a0d918f4154e279e6645
Peter Desmet <peter.desmet@inbo.be>
2024-04-05 14:02:19 +0200

    Pass package in test, not file

--- tests/testthat/test-check_camtrapdp.R
+++ tests/testthat/test-check_camtrapdp.R
@@ -14,7 +14,9 @@ test_that("read_camtrapdp() returns error on invalid Data Package", {
 
 test_that("read_camtrapdp() returns error on invalid camtrapdp object", {
   skip_if_offline()
-  o_assen <- "https://zenodo.org/records/10053903/files/datapackage.json"
+  o_assen <- frictionless::read_package(
+    "https://zenodo.org/records/10053903/files/datapackage.json"
+  )
   expect_error(
     check_camtrapdp(o_assen),
     class = "camtrapdp_error_object_invalid"
