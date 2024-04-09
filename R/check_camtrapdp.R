ebfe2ca70cb92ac95042fd96cc899bdabbdc52dc
Sanne Govaert <44606923+sangovae@users.noreply.github.com>
2024-04-09 13:16:34 +0200

    provide more context with general_message and tip_message

--- R/check_camtrapdp.R
+++ R/check_camtrapdp.R
@@ -10,9 +10,18 @@
 check_camtrapdp <- function(x) {
   frictionless::check_package(x)
   if (!("camtrapdp" %in% class(x))) {
+    general_message <- paste("{.arg x} must be a Camera Trap Data Package",
+                             "object created with {.fun read_camtrapdp}.")
+    tip_message <- paste("Create a valid Camera Trap Data Package object with",
+                         "{.fun read_camtrapdp}.")
+
+    # Check x is a Camera Trap Data Package
     cli::cli_abort(
-      "{.arg x} must be a Camera Trap Data Package object created with
-       {.fun read_camtrapdp}.",
+      c(
+        general_message,
+        "x" = "{.arg x} is a {.cls {class(x)}} object.",
+        "i" = tip_message
+      ),
       class = "camtrapdp_error_object_invalid"
     )
   }
