bf2c9d6848be2b3a8ecfaa63feebcf06f7be44a8
Sanne Govaert <44606923+sangovae@users.noreply.github.com>
2024-04-09 13:27:39 +0200

    remove redundant tests

--- tests/testthat/test-check_camtrapdp.R
+++ tests/testthat/test-check_camtrapdp.R
@@ -5,14 +5,7 @@ test_that("check_camtrapdp() returns x invisibly when valid", {
   expect_invisible(check_camtrapdp(x))
 })
 
-test_that("read_camtrapdp() returns error on invalid Data Package", {
-  expect_error(
-    check_camtrapdp("not_a_package"),
-    class = "frictionless_error_package_invalid"
-  )
-})
-
-test_that("read_camtrapdp() returns error on invalid camtrapdp object", {
+test_that("check_camtrapdp() returns error on invalid camtrapdp object", {
   skip_if_offline()
   o_assen <- frictionless::read_package(
     "https://zenodo.org/records/10053903/files/datapackage.json"
@@ -21,12 +14,5 @@ test_that("read_camtrapdp() returns error on invalid camtrapdp object", {
     check_camtrapdp(o_assen),
     class = "camtrapdp_error_object_invalid"
   )
-  expect_error(
-    check_camtrapdp(o_assen),
-    regexp = paste(
-      "`x` must be a Camera Trap Data Package object created with",
-      "`read_camtrapdp()`."
-    ),
-    fixed = TRUE
-  )
 })
+
