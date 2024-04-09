bd5a8c0370d6f14631eba49b246beaf4191b6942
Sanne Govaert <44606923+sangovae@users.noreply.github.com>
2024-04-09 13:26:55 +0200

    Check that deployments, media and observations data frames are present

--- R/check_camtrapdp.R
+++ R/check_camtrapdp.R
@@ -9,12 +9,13 @@
 #' @noRd
 check_camtrapdp <- function(x) {
   frictionless::check_package(x)
-  if (!("camtrapdp" %in% class(x))) {
-    general_message <- paste("{.arg x} must be a Camera Trap Data Package",
-                             "object created with {.fun read_camtrapdp}.")
-    tip_message <- paste("Create a valid Camera Trap Data Package object with",
-                         "{.fun read_camtrapdp}.")
 
+  general_message <- paste("{.arg x} must be a Camera Trap Data Package",
+                           "object created with {.fun read_camtrapdp}.")
+  tip_message <- paste("Create a valid Camera Trap Data Package object with",
+                       "{.fun read_camtrapdp}.")
+
+  if (!("camtrapdp" %in% class(x))) {
     # Check x is a Camera Trap Data Package
     cli::cli_abort(
       c(
@@ -23,6 +24,46 @@ check_camtrapdp <- function(x) {
         "i" = tip_message
       ),
       class = "camtrapdp_error_object_invalid"
+    )
+  }
+
+  general_message2 <- paste("{.arg x} must have deployments, media and",
+                            "observations data frames present.")
+  # Check deployments is a data frame
+  if(!is.data.frame(x$data$deployments)) {
+    cli::cli_abort(
+      c(
+        general_message2,
+        "x" = "{.arg x} is missing a {.field deployments} attribute OR
+        {.field deployments} is not a data frame.",
+        "i" = tip_message
+      ),
+      class = "camtrapdp_error_data_invalid"
+    )
+  }
+
+  # Check media is a data frame
+  if(!is.data.frame(x$data$media)) {
+    cli::cli_abort(
+      c(
+        general_message2,
+        "x" = "{.arg x} is missing a {.field media} attribute OR
+        {.field media} is not a data frame.",
+        "i" = tip_message
+      ),
+      class = "camtrapdp_error_data_invalid"
+    )
+  }
+  # Check observations is a data frame
+  if(!is.data.frame(x$data$observations)) {
+    cli::cli_abort(
+      c(
+        general_message2,
+        "x" = "{.arg x} is missing a {.field observations} attribute OR
+        {.field observations} is not a data frame.",
+        "i" = tip_message
+      ),
+      class = "camtrapdp_error_data_invalid"
     )
   }
 
