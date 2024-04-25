95e612e2fb7add7f2d35869a842db7c56fbe1771
Peter Desmet <peter.desmet@inbo.be>
2024-04-25 16:05:20 +0200

    Remove check from build_taxonomy()

--- R/build_taxonomy.R
+++ R/build_taxonomy.R
@@ -7,10 +7,6 @@
 #' @return Data frame with the taxonomic information.
 #' @noRd
 build_taxonomy <- function(x) {
-
-  # Check Camtrap DP object
-  check_camtrapdp(x)
-
   # Extract the taxonomic information
   taxonomic_list <- purrr::pluck(x, "taxonomic")
 
