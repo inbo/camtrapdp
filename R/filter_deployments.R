0101a86d004207f76f59d8d0fe83913ff7452f35
Peter Desmet <peter.desmet@inbo.be>
2024-04-26 11:33:19 +0200

    Update documentation and examples for filter_deployments()

--- R/filter_deployments.R
+++ R/filter_deployments.R
@@ -1,40 +1,54 @@
-#' Filter based on deployments
+#' Filter deployments
 #'
-#' Filter a Camera Trap Data Package object based on deployments. Same syntax
-#' and behavior as in dplyr's `filter()` function. The deployments will be filtered based on the provided conditions.
-#' The media and the observations of the removed deployments will be removed as well.
+#' Subsets deployments in a Camera Trap Data Package object, retaining all
+#' rows that satisfy a condition.
+#' - Deployments are filtered on the conditions.
+#' - Media are filtered on associated `deploymentID`.
+#' - Observations are filtered on associated `deploymentID`.
 #'
-#' @param ... filtering expressions. Same behavior as dplyr's `filter()`.
 #' @inheritParams check_camtrapdp
+#' @param ... Filtering conditions, see `dplyr::filter()`.
 #' @return Camera Trap Data Package object.
 #' @family filter functions
 #' @export
 #' @examples
-#' library(dplyr)
-#' dataset <- example_dataset()
-#' filter_deployments(dataset, locationName == "B_HS_val 2_processiepark")
-#' filter_deployments(dataset, deploymentStart > as.POSIXct("2021-01-01"))
-#' # piping is allowed
-#' dataset %>% filter_deployments(latitude > 51, longitude < 5)
+#' x <- example_dataset()
+#'
+#' # Filtering returns x (the dataset)
+#' filter_deployments(x, deploymentID == "62c200a9")
+#'
+#' # Use deployments() to get the filtered deployments
+#' x %>%
+#'   filter_deployments(deploymentID == "62c200a9") %>%
+#'   deployments()
+#'
+#' # Filtering by multiple criteria
+#' x %>%
+#'   filter_deployments(latitude > 51.0 & longitude > 5.0) %>%
+#'   deployments()
+#'
+#' # Associated media and observations are filtered as well
+#' x_filtered <- filter_deployments(x, deploymentID == "62c200a9")
+#' media(x_filtered)
+#' observations(x_filtered)
 filter_deployments <- function(x, ...) {
-
-  # Check Camtrap DP object
   check_camtrapdp(x)
 
-  # Filter deployments
-  deploys <- deployments(x)
-  deploys <- dplyr::filter(deploys, ...)
+  # Filter data
+  deployments <-
+    deployments(x) %>%
+    dplyr::filter(...)
+  media <-
+    media(x) %>%
+    dplyr::filter(.data$deploymentID %in% deployments$deploymentID)
+  observations <-
+    observations(x) %>%
+    dplyr::filter(.data$deploymentID %in% deployments$deploymentID)
 
-  obs <- observations(x) %>%
-    dplyr::filter(.data$deploymentID %in% deploys$deploymentID)
-
-  media <- media(x) %>%
-    dplyr::filter(.data$deploymentID %in% deploys$deploymentID)
-
-  # Assign filtered data to deployments, observations, media
-  x$data$deployments <- deploys
-  x$data$observations <- obs
+  # Assign filtered data
+  x$data$deployments <- deployments
   x$data$media <- media
+  x$data$observations <- observations
 
   return(x)
 }
