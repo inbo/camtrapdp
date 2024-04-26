e359925df5b326a1cc9e4002e9926ec1c9f4ce6b
Peter Desmet <peter.desmet@inbo.be>
2024-04-26 15:54:14 +0200

    Document automatic assignment

--- R/read_camtrapdp.R
+++ R/read_camtrapdp.R
@@ -3,6 +3,34 @@
 #' Reads files from a [Camera Trap Data Package (Camtrap DP)](
 #' https://camtrap-dp.tdwg.org) into memory.
 #'
+#' @section Assign taxonomic information:
+#'
+#' Camtrap DP metadata has a `taxonomic` property that can contain extra
+#' information for each `scientificName` found in observations.
+#' Such information can include higher taxonomy (`family`, `order`, etc.) and
+#' vernacular names in multiple languages.
+#'
+#' This function **will automatically include this taxonomic information in
+#' observations**, as extra columns starting with `taxon.`.
+#'
+#' @section Assign eventIDs:
+#'
+#' Observations can contain two classifications at two levels:
+#'
+#' **Media-based** observations (`observationLevel == "media"`) are based on a
+#' single media file and are directly linked to it via `mediaID`.
+#'
+#' **Event-based** observations (`observationLevel == "event"`) are based an
+#' event, defined as a combination of `eventID`, `eventStart` and `eventID`.
+#' This event can consist of one or more media files, but is not directly linked
+#' to these
+#'
+#' This function **will automatically assign `eventID`s to media**, using
+#' `media.deploymentID == event.deploymentID` and `eventStart <= media.timestamp
+#' <= eventEnd`.
+#' Note that this can result in media being linked to multiple events (and thus
+#' being duplicated), for example when events and sub-events were defined.
+#'
 #' @param file Path or URL to a `datapackage.json` file.
 #' @return Camera Trap Data Package object.
 #' @family read functions
@@ -40,19 +68,6 @@ read_camtrapdp <- function(file) {
   # Convert
   x <- convert(x, convert_to = "1.0")
 
-  # Add eventID to media
-  x$data$media <-
-    dplyr::left_join(
-      media(x),
-      events(x),
-      by = dplyr::join_by(
-        "deploymentID",
-        "timestamp" >= "eventStart",
-        "timestamp" <= "eventEnd"
-      )
-    ) %>%
-    dplyr::select(-"eventStart", -"eventEnd")
-
   # Add taxonomic info to observations
   taxonomy <- build_taxonomy(x)
   if (!is.null(taxonomy)) {
@@ -67,6 +82,19 @@ read_camtrapdp <- function(file) {
         by = dplyr::join_by("scientificName" == "taxon.scientificName")
       )
   }
+
+  # Add eventID to media
+  x$data$media <-
+    dplyr::left_join(
+      media(x),
+      events(x),
+      by = dplyr::join_by(
+        "deploymentID",
+        "timestamp" >= "eventStart",
+        "timestamp" <= "eventEnd"
+      )
+    ) %>%
+    dplyr::select(-"eventStart", -"eventEnd")
 
   return(x)
 }
