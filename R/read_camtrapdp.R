4e473e3c31e9ebbb00cea3de10c48c9c8cd5df5d
Peter Desmet <peter.desmet@inbo.be>
2024-04-26 15:02:32 +0200

    Assign eventIDs to media in read_camtrapdp

--- R/read_camtrapdp.R
+++ R/read_camtrapdp.R
@@ -4,10 +4,12 @@
 #' https://camtrap-dp.tdwg.org) into memory.
 #'
 #' @param file Path or URL to a `datapackage.json` file.
+#' @param media_eventid Whether `eventID`s found in observations should be
+#'   assigned to media.
 #' @return Camera Trap Data Package object.
 #' @family read functions
 #' @export
-read_camtrapdp <- function(file) {
+read_camtrapdp <- function(file, media_eventid = TRUE) {
   # Read datapackage.json
   package <- suppressMessages(frictionless::read_package(file))
 
@@ -40,18 +42,34 @@ read_camtrapdp <- function(file) {
   # Convert
   x <- convert(x, convert_to = "1.0")
 
-  # Add taxonomy
+  # Add eventID to media
+  if (media_eventid) {
+    x$data$media <-
+      dplyr::left_join(
+        media(x),
+        events(x),
+        by = dplyr::join_by(
+          "deploymentID",
+          "timestamp" >= "eventStart",
+          "timestamp" <= "eventEnd"
+        )
+      ) %>%
+      dplyr::select(-"eventStart", -"eventEnd")
+  }
+
+  # Add taxonomic info to observations
   taxonomy <- build_taxonomy(x)
   if (!is.null(taxonomy)) {
     # Add taxon. as column suffix
     colnames(taxonomy) <- paste("taxon", colnames(taxonomy), sep = ".")
 
     # Join taxonomy with observations
-    x$data$observations <- dplyr::left_join(
-      observations(x),
-      taxonomy,
-      by = dplyr::join_by("scientificName" == "taxon.scientificName")
-    )
+    x$data$observations <-
+      dplyr::left_join(
+        observations(x),
+        taxonomy,
+        by = dplyr::join_by("scientificName" == "taxon.scientificName")
+      )
   }
 
   return(x)
--- tests/testthat/test-read_camtrapdp.R
+++ tests/testthat/test-read_camtrapdp.R
@@ -3,7 +3,6 @@ test_that("read_camtrapdp() reads a Camtrap DP", {
   file <-
     "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
   expect_no_error(read_camtrapdp(file))
-
   # TODO: add more tests on returned object
 })
 
@@ -32,4 +31,26 @@ test_that("read_camtrapdp() does not convert 1.0", {
     "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
   x <- read_camtrapdp(camtrapdp_1.0)
   expect_identical(version(x), "1.0")
+})
+
+test_that("read_camtrapdp() optionally adds eventIDs to media", {
+  skip_if_offline()
+  file <-
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/datapackage.json"
+
+  # Don't assign eventID
+  x_without_eventid <- read_camtrapdp(file, media_eventid = FALSE)
+  expect_true(!"eventID" %in% colnames(media(x_without_eventid)))
+
+  # Assign eventID
+  x <- read_camtrapdp(file, media_eventid = TRUE)
+  expect_true("eventID" %in% colnames(media(x)))
+  expect_identical(
+    nrow(media(x)),
+    nrow(media(x_without_eventid)) # No duplicates are created in example dataset
+  )
+  expect_equal(
+    media(x) %>% dplyr::filter(is.na(eventID)) %>% nrow(),
+    3 # Only 3 (timeLapse) media don't have an eventID assigned
+  )
 })
