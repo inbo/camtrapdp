d7f301b51f8f165dfcec92578c3ea34a7e73d9a1
Peter Desmet <peter.desmet@inbo.be>
2024-05-21 11:26:48 +0200

    Update tests and remove write_dwc_snapshot helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,23 +1,3 @@
-#' Get snapshot for write_dwc()
-#'
-#' Wrapper of `write_dwc()` that returns path of selected output file.
-#' Needed for `testthat::expect_snapshot_file()` which expects the path of a
-#' single file to compare against snapshot.
-#' @inheritParams write_dwc
-#' @param file Either `occurrence` or `audiovisual` to select which output file of
-#'   `write_dwc()` to return.
-#' @return Path of selected output file.
-#' @noRd
-#' @examples write_dwc_snapshot(mica, tempdir(), "occurrence")
-write_dwc_snapshot <- function(package, directory, file){
-  suppressMessages(write_dwc(package, directory))
-  switch(
-    file,
-    occurrence = file.path(directory, "dwc_occurrence.csv"),
-    audiovisual = file.path(directory, "dwc_audiovisual.csv")
-  )
-}
-
 #' testthat wrapper to compare DwC-A files against meta.xml file for fieldsnames
 #'
 #' @param file Filepath from of file from DwC-A file to compare against
