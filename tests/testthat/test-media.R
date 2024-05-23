f2b2dd5448457cb695799411455acc87e82a20fc
PietrH <pieterhuy@gmail.com>
2024-05-23 10:45:17 +0200

    cookiecutter tests from `deployments()` assignment

--- tests/testthat/test-media.R
+++ tests/testthat/test-media.R
@@ -12,3 +12,48 @@ test_that("media() returns the media", {
     x$data$media
   )
 })
+
+test_that("media() <- allows assignment of data.frames to media", {
+  skip_if_offline()
+  y <- example_dataset()
+  test_df <- data.frame(a = 1:3)
+  # overwrite the media of example dataset with a data.frame
+  media(y) <- test_df
+
+  expect_identical(
+    y$data$media,
+    test_df
+  )
+
+  expect_identical(
+    media(y),
+    test_df
+  )
+})
+
+test_that("media() <- returns the object after assignment", {
+  skip_if_offline()
+  y <- example_dataset()
+  test_df <- data.frame(a = 1:3)
+
+  expect_identical(
+    media(y) <- test_df,
+    test_df
+  )
+})
+
+test_that("media() <- errors when you try to assign a non data.frame", {
+  skip_if_offline()
+  y <- example_dataset()
+
+  expect_error(
+    media(y) <- "not a data.frame object",
+    class = "camtrapdp_error_assignment_wrong_class"
+  )
+
+  expect_error(
+    media(y) <- matrix(1:6,2,3),
+    regexp = "`value` is a an integer matrix but needs to be a `data.frame`",
+    fixed = TRUE
+  )
+})
--- tests/testthat/test-observations.R
+++ tests/testthat/test-observations.R
@@ -12,3 +12,48 @@ test_that("observations() returns the observations", {
     x$data$observations
   )
 })
+
+test_that("observations() <- allows assignment of data.frames to observations", {
+  skip_if_offline()
+  y <- example_dataset()
+  test_df <- data.frame(a = 1:3)
+  # overwrite the observations of example dataset with a data.frame
+  observations(y) <- test_df
+
+  expect_identical(
+    y$data$observations,
+    test_df
+  )
+
+  expect_identical(
+    observations(y),
+    test_df
+  )
+})
+
+test_that("observations() <- returns the object after assignment", {
+  skip_if_offline()
+  y <- example_dataset()
+  test_df <- data.frame(a = 1:3)
+
+  expect_identical(
+    observations(y) <- test_df,
+    test_df
+  )
+})
+
+test_that("observations() <- errors when you try to assign a non data.frame", {
+  skip_if_offline()
+  y <- example_dataset()
+
+  expect_error(
+    observations(y) <- "not a data.frame object",
+    class = "camtrapdp_error_assignment_wrong_class"
+  )
+
+  expect_error(
+    observations(y) <- matrix(1:6,2,3),
+    regexp = "`value` is a an integer matrix but needs to be a `data.frame`",
+    fixed = TRUE
+  )
+})
