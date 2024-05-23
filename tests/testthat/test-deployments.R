f649e19355221f2d85291229aec936a7cfe62e7e
PietrH <pieterhuy@gmail.com>
2024-05-23 10:22:20 +0200

    test if assignment works

--- tests/testthat/test-deployments.R
+++ tests/testthat/test-deployments.R
@@ -14,7 +14,21 @@ test_that("deployments() returns the deployments", {
 })
 
 test_that("deployments() <- allows assignment of data.frames to deployments", {
+  skip_if_offline()
+  y <- example_dataset()
+  test_df <- data.frame(a = 1:3)
+  # overwrite the deployments of example dataset with a data.frame
+  deployments(y) <- test_df
 
+  expect_identical(
+    y$data$deployments,
+    test_df
+  )
+
+  expect_identical(
+    deployments(y),
+    test_df
+  )
 })
 
 test_that("deployments() <- returns the object after assignment", {
