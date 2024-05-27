2e9764570dea77c9759f4715258d034e9cf0e98e
Peter Desmet <peter.desmet@inbo.be>
2024-05-27 15:01:06 +0200

    Use unmemoised .example_dataset
    Looks more how a function is expected to look + it's easy to switch between memoised and unmemoised while developing: just comment example_dataset and rename .example_dataset -> example_dataset
    
    Inspired by:
    
    https://github.com/r-lib/devtools/blob/c3fa3dfb8de4e37b573cccff76f8c35a65a4488b/R/sitrep.R#L66-L70
    https://github.com/ropensci/codemetar/blob/26a6bbdd95cb17199e20630ddc8b1e24d41adfb2/R/extract_badges.R#L84-L106

--- R/example_dataset.R
+++ R/example_dataset.R
@@ -8,10 +8,12 @@
 #' @export
 #' @examples
 #' example_dataset()
-example_dataset <- memoise::memoise(function() {
-  camtrapdp_1.0 <- file.path(
+.example_dataset <- function() {
+  url <- file.path(
     "https://raw.githubusercontent.com/tdwg/camtrap-dp",
     "1.0/example/datapackage.json"
   )
-  read_camtrapdp(camtrapdp_1.0)
-})
+  read_camtrapdp(url)
+}
+
+example_dataset <- memoise::memoise(.example_dataset)
