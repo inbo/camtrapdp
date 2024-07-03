79047ede2a293155534f854fa5cf45f6ab72e2de
PietrH <pieterhuy@gmail.com>
2024-07-03 14:46:10 +0200

    Add a print method for Camera Trap Data Packages

---
+++ R/print.R
@@ -0,0 +1,44 @@
+#' Print a Camera Trap Data Package
+#'
+#' Prints a human-readable summary of a Camera Trap Data Package, as an
+#' extension of the printing provided by the frictionless package. This summary
+#' provides the most important aspects of a Camera Trap Data Package.
+#'
+#' @param x Camera Trap Data Package object, as returned by
+#'   `read_camtrapdp()`.
+#' @param ...
+#'
+#' @return [print()] with a summary of the Camera Trap Data Package object.
+#' @export
+#'
+#' @examples
+#' dp <- example_dataset()
+#' print(example_dataset)
+print.camtrapdp <- function(x, ...) {
+  # Check if the Camera Trap Data Package object is valid
+  check_camtrapdp(x)
+
+  # List the resources
+
+  # List resources
+  resources <- frictionless::resources(x)
+  cli::cat_line(
+    cli::format_inline(
+      "A Data Package with {length(resources)} resource{?s}{?./:/:}"
+    )
+  )
+
+  # Calculate the number of rows of every resource in `x$data`
+  nrow_summary <-
+    purrr::pluck(x, "data") %>%
+    purrr::map(nrow)
+
+  # Print out a summary of the Camera Trap Data Package object.
+  purrr::walk2(
+    names(nrow_summary), # The names of the resources
+    nrow_summary, # The number of rows in these resource objects
+    ~ cli::cat_bullet(cli::format_inline("{.x}: {.y}"))
+  )
+
+  invisible(x)
+}
