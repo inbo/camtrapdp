66287056f79209f81ec10e655b113eefe9404179
PietrH <pieterhuy@gmail.com>
2024-07-03 14:54:55 +0200

    Add `print()` test skeletons

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,3 +1,16 @@
 test_that("print() returns output invisibly", {
 
 })
+
+test_that("print() informs about the number of resources", {
+
+})
+
+test_that("print() informs about the number of rows for every resource", {
+
+})
+
+test_that("print() can handle an input with non standard numer of resources", {
+
+})
+
