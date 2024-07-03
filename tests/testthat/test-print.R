2540003ef35aeee1588a6e06906a4ae9849f7214
PietrH <pieterhuy@gmail.com>
2024-07-03 16:42:56 +0200

    test for invisible output

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,5 +1,8 @@
 test_that("print() returns output invisibly", {
-
+  # Test that there is any output at all
+  expect_output(output <- withVisible(print(example_dataset())))
+  # Test that the value (the returned object) does not auto print
+  expect_false(output$visible)
 })
 
 test_that("print() informs about the number of resources", {
