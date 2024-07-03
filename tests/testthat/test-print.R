413ecd02d4563561f4e2a4614d49175ba8984115
PietrH <pieterhuy@gmail.com>
2024-07-03 16:43:10 +0200

    test for number of resources

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -6,7 +6,31 @@ test_that("print() returns output invisibly", {
 })
 
 test_that("print() informs about the number of resources", {
+  # Test that information is provided about the number of resources
+  expect_output(
+    print(example_dataset()),
+    regexp = "A Data Package with [0-9]+ resources:",
+    fixed = FALSE
+  )
+  # Test for 5 resources
+  five_resources <- example_dataset()
+  purrr::pluck(five_resources, "resources") <-
+    sample(five_resources$resources, 5, replace = TRUE)
 
+  expect_output(
+    print(five_resources),
+    regexp = "A Data Package with 5 resources:",
+    fixed = TRUE
+  )
+  # Test 0 resources
+  zero_resources <- example_dataset()
+  purrr::pluck(zero_resources, "resources") <- list()
+
+  expect_output(
+    print(five_resources),
+    regexp = "A Data Package with 0 resources:",
+    fixed = TRUE
+  )
 })
 
 test_that("print() informs about the number of rows for every resource", {
