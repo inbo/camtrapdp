088727a6bedc6ead33d267fffe997f0d28b09798
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-07-05 12:35:20 +0200

    If there are no deployments, set spatial to NULL

--- R/utils.R
+++ R/utils.R
@@ -7,22 +7,24 @@
 #' @family helper functions
 #' @noRd
 build_spatial <- function(x) {
-  # get bounding box data
-  long_max <- max(deployments(x)$longitude)
-  long_min <- min(deployments(x)$longitude)
-  lat_max <- max(deployments(x)$latitude)
-  lat_min <- min(deployments(x)$latitude)
+  if (nrow(deployments(x)) == 0) {
+    x$spatial <- NULL
+  } else { # otherwise get bounding box data
+    long_max <- max(deployments(x)$longitude)
+    long_min <- min(deployments(x)$longitude)
+    lat_max <- max(deployments(x)$latitude)
+    lat_min <- min(deployments(x)$latitude)
 
-  x$spatial$bbox <- c(long_min, lat_min, long_max, lat_max)
+    x$spatial$bbox <- c(long_min, lat_min, long_max, lat_max)
 
-  x$spatial$coordinates <-
-    array(
-      c(
-        long_min, long_max, long_max, long_min, long_min,
-        lat_min, lat_min, lat_max, lat_max, lat_min
-      ),
-      dim = c(1, 5, 2)
-    )
-
+    x$spatial$coordinates <-
+      array(
+        c(
+          long_min, long_max, long_max, long_min, long_min,
+          lat_min, lat_min, lat_max, lat_max, lat_min
+        ),
+        dim = c(1, 5, 2)
+      )
+  }
   return(x)
 }
