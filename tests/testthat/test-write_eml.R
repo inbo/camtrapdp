a6ca41b00bf86740b313b4725ad5773e8c6cda01
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-07-08 10:34:38 +0200

    update test to increase coverage

--- tests/testthat/test-write_eml.R
+++ tests/testthat/test-write_eml.R
@@ -1,25 +1,20 @@
-# Create temporary files
-temp_dir <- file.path(tempdir(), "camtrapdp")
-dir.create(temp_dir)
-
-test_that("write_eml() can write an eml", {
+test_that("write_eml() writes an eml to the given directory", {
   skip_if_offline()
   x <- example_dataset()
   # compare against known good result
   eml <-
-    suppressMessages(write_eml(x, directory = temp_dir))
+    suppressMessages(write_eml(x, directory = NULL))
   ## don't compare the packageId because it's a random guid
   purrr::pluck(eml, "packageId") <- NULL
   expect_snapshot(eml)
 
   # write to temp dir
-  suppressMessages(expect_no_error(write_eml(x, directory = temp_dir)))
+  suppressMessages(expect_no_error(write_eml(x, directory = tempdir())))
   ## read from file, and remove packageID because it's a random guid
-  eml_from_file <- EML::read_eml(file.path(temp_dir, "eml.xml"))
+  eml_from_file <- EML::read_eml(file.path(tempdir(), "eml.xml"))
   purrr::pluck(eml_from_file, "packageId") <- NULL
   ## compare to known output
   expect_snapshot(eml_from_file)
+  ## remove temp file
+  unlink(file.path(tempdir(), "eml.xml"))
 })
-
-# Remove temporary files
-unlink(temp_dir, recursive = TRUE)
