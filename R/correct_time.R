312cb18bd79208eba04e1d4a6abbd54fabe359e3
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-07-17 16:16:41 +0200

    add function correct_time()

---
+++ R/correct_time.R
@@ -0,0 +1,79 @@
+#' Corrects date-time
+#'
+#' Corrects date-times in a Camera Trap Data package object. This function can
+#' be used when the time settings of one or more deployments were wrong and need
+#' to be corrected afterwards.
+#'
+#' @inheritParams print.camtrapdp
+#' @param deploymentID One or more deploymentID's.
+#' @param duration Date-time difference between the wrong and right time.
+#' @return `x` with date-times corrected.
+#' @family transformation functions
+#' @export
+#' @examples
+#' x <- example_dataset()
+#' deploymentID <- c("00a2c20d", "29b7d356")
+#' # calculate duration
+#' library(lubridate)
+#' wrong <- ymd_hms("2024-04-01T00:00:00", tz = "UTC")
+#' right <- ymd_hms("2024-04-01T02:00:00", tz = "UTC")
+#' duration <- as.duration(interval(wrong, right))
+#' # correct time
+#' x_corrected <- correct_time(x, deploymentID, duration)
+#' # inspect results
+#' deployments(x_corrected)
+correct_time <- function(x, deploymentID, duration) {
+  check_camtrapdp(x)
+
+  # correct deploymentStart and deploymentEnd of selected deployments
+  x$data$deployments <-
+    deployments(x) %>%
+    dplyr::mutate(
+      deploymentStart =
+        ifelse(
+          deploymentID %in% deploymentID,
+          deploymentStart + duration,
+          deploymentStart
+        ),
+      deploymentEnd =
+        ifelse(
+          deploymentID %in% deploymentID,
+          as.POSIXct(deploymentEnd + duration),
+          as.POSIXct(deploymentEnd)
+        )
+    )
+
+  x$data$deployments
+
+  # correct eventStart and eventEnd of associated observations
+  x$data$observations <-
+    observations(x) %>%
+    dplyr::mutate(
+      eventStart =
+        ifelse(
+          deploymentID %in% deploymentID,
+          eventStart + duration,
+          eventStart
+        ),
+      eventEnd =
+        ifelse(
+          deploymentID %in% deploymentID,
+          eventEnd + duration,
+          eventEnd
+        )
+    )
+
+  # correct timestamp of associated media
+  x$data$media <-
+    media(x) %>%
+    dplyr::mutate(
+      timestamp =
+        ifelse(
+          deploymentID %in% deploymentID,
+          timestamp + duration,
+          timestamp
+        )
+    )
+
+  return(x)
+}
