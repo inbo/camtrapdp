ffa9ee6ae159bce34825f0be5b662f47044a76bd
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-07-23 17:07:51 +0200

    new function merge_camtrapdp()

---
+++ R/merge_camtrapdp.R
@@ -0,0 +1,72 @@
+#' Merge camtrapdp packages
+#'
+#' @param x1
+#' @param x2
+#' @param name
+#' @param title
+#' @return `x`
+#' @export
+#' @examples
+#' x <- example_dataset() %>%
+#'   filter_deployments(deploymentID %in% c("00a2c20d", "29b7d356"))
+#' x2 <- example_dataset() %>%
+#'   filter_deployments(deploymentID %in% c("577b543a", "62c200a9"))
+#' x_merged <- merge_camtrapdp(x1, x2, "new package name", "new title")
+merge_camtrapdp <- function(x1, x2, name, title) {
+  check_camtrapdp(x1)
+  check_camtrapdp(x2)
+
+  x <- x1
+
+  # check duplicated ID's
+
+
+  # merge resources
+  deployments(x) <- dplyr::bind_rows(deployments(x1), deployments(x2))
+  media(x) <- dplyr::bind_rows(media(x1), media(x2))
+  observations(x) <- dplyr::bind_rows(observations(x1), observations(x2))
+
+  # merge/update mtadata
+  x$name <- name
+  x$id <- digest::digest(paste(x$title, x2$title), algo = "md5")
+  x$created <- format(Sys.time(), "%Y-%m-%dT%H:%M:%SZ")
+  x$title <- title
+  x$contributors <- c(x1$contributors, x2$contributors)
+  paragraph <- paste0(
+    "This dataset is a combination of 2 datasets: ", x1$title, "and", x2$title,
+    ".")
+  x$version <- "1.0"
+  x$keywords <- unique(x1$keywords, x2$keywords)
+  x$image <- NULL
+  x$homepage <- NULL
+  x$sources <- c(x1$sources, x2$sources)
+  x$licenses <- c(x1$licenses, x2$licences)
+  x$bibliographicCitation <- NULL
+  x$coordinatePrecision <-
+    max(x1$coordinatePrecision, x2$coordinatePrecision, na.rm = TRUE)
+
+  relatedIdentifiers_x1 <- list(
+    relationType = "IsDerivedFrom",
+    relatedIdentifier = x1$id,
+    resourceTypeGeneral = "Data package",
+    relatedIdentifierType = "id"
+  )
+  relatedIdentifiers_x2 <- list(
+    relationType = "IsDerivedFrom",
+    relatedIdentifier = x2$id,
+    resourceTypeGeneral = "Data package",
+    relatedIdentifierType = "id"
+  )
+  new_relatedIdentifiers <- list(relatedIdentifiers_x1, relatedIdentifiers_x2)
+  x$relatedIdentifiers <-
+    c(x1$relatedIdentifiers, x2$relatedIdentifiers, new_relatedIdentifiers)
+
+  x$references <- c(x1$references, x2$references)
+
+  x %>%
+    update_spatial(x) %>%
+    update_temporal() %>%
+    update_taxonomic()
+
+  return(x)
+}
