04afa530f3921501927bb50efcd98d61693c6fd7
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-07-24 17:33:55 +0200

    add test "merge_camtrapdp() returns no duplicated deploymentID's"

--- tests/testthat/test-merge_camtrapdp.R
+++ tests/testthat/test-merge_camtrapdp.R
@@ -8,3 +8,20 @@ test_that("merge_camtrapdp() returns a valid camtrapdp object", {
     check_camtrapdp(merge_camtrapdp(x1, x2, "new package name", "new title"))
   )
 })
+
+test_that("merge_camtrapdp() returns no duplicated deploymentID's", {
+  skip_if_offline()
+  x1 <- example_dataset()
+  x2 <- example_dataset() %>%
+    filter_deployments(deploymentID %in% c("00a2c20d"))
+  original_deploymentIDs <- c(
+    purrr::pluck(deployments(x1), "deploymentID"),
+    purrr::pluck(deployments(x2), "deploymentID")
+  )
+  x_merged <-
+    check_camtrapdp(merge_camtrapdp(x1, x1, "new package name", "new title"))
+  new_deploymentIDs <- purrr::pluck(deployments(x_merged), "deploymentID")
+
+  expect_true(any(duplicated(original_deploymentIDs)))
+  expect_false(any(duplicated(new_deploymentIDs)))
+})
