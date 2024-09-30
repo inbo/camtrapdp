4386be9be794389152c2a7c4cc521c60852b276d
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-09-30 17:22:47 +0200

    add tests (work in progress)

--- tests/testthat/test-merge_camtrapdp.R
+++ tests/testthat/test-merge_camtrapdp.R
@@ -67,25 +67,36 @@ test_that("merge_camtrapdp() adds prefixes to all values of identifiers
   x2 <- example_dataset()
   x1$id <- "1"
   x2$id <- "2"
+
+  # Default prefixes
+  x_merged_default <- merge_camtrapdp(x1, x2)
+  expect_true("100a2c20d" %in% deployments(x_merged)$deploymentID)
+  expect_true("200a2c20d" %in% deployments(x_merged)$deploymentID)
+
+  # Custom prefixes
   x_merged <- merge_camtrapdp(x1, x2, prefix = c("project1-", "project2-"))
 
+  # deploymentID
   expect_true("project1-00a2c20d" %in% deployments(x_merged)$deploymentID)
   expect_true("project2-00a2c20d" %in% deployments(x_merged)$deploymentID)
   expect_true("project1-00a2c20d" %in% media(x_merged)$deploymentID)
   expect_true("project1-00a2c20d" %in% observations(x_merged)$deploymentID)
 
+  # mediaID
   expect_true("project1-07840dcc" %in% media(x_merged)$mediaID)
   expect_true("project1-07840dcc" %in% observations(x_merged)$mediaID)
   expect_false("project1-NA" %in% observations(x_merged)$mediaID)
   expect_true(NA %in% observations(x_merged)$mediaID)
 
+  # observationID
   expect_true("project1-705e6036" %in% observations(x_merged)$observationID)
 
+  # eventID
   expect_true("project1-4bb69c45" %in% media(x_merged)$eventID)
   expect_true("project1-4bb69c45" %in% observations(x_merged)$eventID)
 })
 
-test_that("merge_camtrapdp() returns the expected result", {
+test_that("merge_camtrapdp() returns the expected metadata", {
   skip_if_offline()
   x1 <- example_dataset()
   x2 <- example_dataset()
@@ -146,4 +157,112 @@ test_that("merge_camtrapdp() returns the expected result", {
   expect_identical(x_merged$relatedIdentifiers, relatedIdentifiers_merged)
 
   # Check data
+})
+
+test_that("merge_camtrapdp() does x and y", {
+  skip_if_offline()
+  x1 <- example_dataset()
+
+  temp_dir <- tempdir()
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  zip_file <- file.path(temp_dir, "dataset.zip")
+  datapackage_file <- file.path(temp_dir, "datapackage.json")
+  url <- "https://ipt.nlbif.nl/archive.do?r=awd_pilot2"
+
+  download.file(url, zip_file, mode = 'wb')
+  unzip(zip_file, exdir = temp_dir)
+
+  x2 <- read_camtrapdp(datapackage_file)
+
+  x_merged <- merge_camtrapdp(x1, x2)
+
+  # Check metadata
+  profile <- "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0.1/camtrap-dp-profile.json"
+  contributors <- list(
+    list(
+      title = "Axel Neukermans",
+      email = "axel.neukermans@inbo.be",
+      path = "https://orcid.org/0000-0003-0272-9180",
+      role = "contributor",
+      organization = "Research Institute for Nature and Forest (INBO)"
+    ),
+    list(
+      title = "Danny Van der beeck",
+      email = "daniel.vanderbeeck@gmail.com"
+    ),
+    list(
+      title = "Emma Cartuyvels",
+      email = "emma.cartuyvels@inbo.be",
+      role = "principalInvestigator",
+      organization = "Research Institute for Nature and Forest (INBO)"
+    ),
+    list(
+      title = "Peter Desmet",
+      email = "peter.desmet@inbo.be",
+      path = "https://orcid.org/0000-0002-8442-8025",
+      role = "contact",
+      organization = "Research Institute for Nature and Forest (INBO)"
+    ),
+    list(
+      title = "Research Institute for Nature and Forest (INBO)",
+      path = "https://inbo.be",
+      role = "rightsHolder"
+    ),
+    list(
+      title = "Research Institute for Nature and Forest (INBO)",
+      path = "https://inbo.be",
+      role = "publisher"
+    ),
+    list(
+      title = "Julian Evans",
+      email = "jevansbio@gmail.com",
+      role = "principalInvestigator",
+      organization = "University of Amsterdam",
+      firstName = "Julian",
+      lastName = "Evans"
+    ),
+    list(
+      title = "Rotem Zilber",
+      email = "r.kadanzilber@uva.nl",
+      role = "principalInvestigator",
+      organization = "University of Amsterdam",
+      firstName = "Rotem",
+      lastName = "Zilber"
+    ),
+    list(
+      title = "W. Daniel  Kissling",
+      email = "wdkissling@gmail.com",
+      path = "https://www.danielkissling.de/",
+      role = "principalInvestigator",
+      organization = "University of Amsterdam",
+      firstName = "W. Daniel ",
+      lastName = "Kissling"
+    )
+  )
+
+  expect_identical(x_merged$resources, x1$resources)
+  expect_identical(x_merged$profile, profile)
+  expect_identical(x_merged$name, NA)
+  expect_identical(x_merged$id, NA)
+  expect_identical(x_merged$title, NA)
+  expect_identical(x_merged$contributors, contributors)
+  # no test for description
+  expect_identical(x_merged$version, "1.0")
+  expect_identical(x_merged$keywords, c(x1$keywords, x2$keywords))
+  expect_identical(x_merged$image, NULL)
+  expect_identical(x_merged$homepage, NULL)
+  expect_identical(x_merged$sources, sources)
+  expect_equal(x_merged$licenses, licenses) # fails because remove_duplicates switches order of subelements
+  expect_identical(x_merged$bibliographicCitation, NULL)
+  expect_identical(x_merged$projects, list(project, x2$project))
+  expect_identical(x_merged$coordinatePrecision, coordinatePrecision)
+  expect_identical(x_merged$spatial, spatial)
+  expect_identical(x_merged$temporal, temporal)
+  expect_identical(x_merged$taxonomic, taxonomic)
+  expect_identical(x_merged$references, references)
+  expect_identical(x_merged$directory, directory)
+
+  relatedIdentifiers_merged <- list()
+
+  expect_identical(x_merged$relatedIdentifiers, relatedIdentifiers_merged)
 })
