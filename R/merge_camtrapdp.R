c3e874cf6d74ede4ff7f2ff6f25ba83e01f32762
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-10-08 12:01:02 +0200

    update parameter names

--- R/merge_camtrapdp.R
+++ R/merge_camtrapdp.R
@@ -1,8 +1,8 @@
 #' Merge Camera Trap Data packages
 #'
-#' @param x1,x2 Camera Trap Data Package objects (as returned by
+#' @param x,y Camera Trap Data Package objects (as returned by
 #' `read_camtrapdp()`), to be coerced to one.
-#' @param prefix If there are duplicate IDs in x1 an x2, these prefixes will be
+#' @param prefix If there are duplicate IDs in x an y, these prefixes will be
 #' added to all the values of each identifier with duplicates, to disambiguate
 #' them. Should be a character vector of length 2. By default, the prefixes are
 #' the ID's of the Data Package.
@@ -10,24 +10,24 @@
 #' @family transformation functions
 #' @export
 #' @examples
-#' x1 <- example_dataset() %>%
+#' x <- example_dataset() %>%
 #'   filter_deployments(deploymentID %in% c("00a2c20d", "29b7d356"))
-#' x2 <- example_dataset() %>%
+#' y <- example_dataset() %>%
 #'   filter_deployments(deploymentID %in% c("577b543a", "62c200a9"))
-#' x_merged <- merge_camtrapdp(x1, x2)
-merge_camtrapdp <- function(x1, x2, prefix = c(x1$id, x2$id)) {
-  check_camtrapdp(x1)
-  check_camtrapdp(x2)
+#' merged_xy <- merge_camtrapdp(x, y)
+merge_camtrapdp <- function(x, y, prefix = c(x$id, y$id)) {
+  check_camtrapdp(x)
+  check_camtrapdp(y)
 
-  if (!is.null(x1$id) & !is.null(x2$id)) {
-    if (x1$id == x2$id) {
+  if (!is.null(x$id) & !is.null(y$id)) {
+    if (x$id == y$id) {
       cli::cli_abort(
         c(
           paste0(
-            "{.arg x1} and {.arg x2} should be different Camera Trap Data",
+            "{.arg x} and {.arg y} should be different Camera Trap Data",
             "Package objects with unique identifiers."
           ),
-          x = "{.arg x1} and {.arg x2} have the same id: {.value x1$id}"
+          x = "{.arg x} and {.arg y} have the same id: {.value x$id}"
         ),
         class = "camtrapdp_error_camtrapdpid_duplicated"
       )
@@ -35,7 +35,7 @@ merge_camtrapdp <- function(x1, x2, prefix = c(x1$id, x2$id)) {
   }
 
   # check if identifiers have duplicates
-  results_duplicate_ids <- check_duplicate_ids(x1, x2)
+  results_duplicate_ids <- check_duplicate_ids(x, y)
 
   # Add prefix to identifiers with duplicates
   if (TRUE %in% results_duplicate_ids) {
@@ -59,61 +59,61 @@ merge_camtrapdp <- function(x1, x2, prefix = c(x1$id, x2$id)) {
       )
     }
 
-    x1 <- add_prefix(x1, results_duplicate_ids, paste0(prefix[1], "_"))
-    x2 <- add_prefix(x2, results_duplicate_ids, paste0(prefix[2], "_"))
+    x <- add_prefix(x, results_duplicate_ids, paste0(prefix[1], "_"))
+    y <- add_prefix(y, results_duplicate_ids, paste0(prefix[2], "_"))
   }
 
   # Merge resources
-  x <- x1
-  deployments(x) <- dplyr::bind_rows(deployments(x1), deployments(x2))
-  media(x) <- dplyr::bind_rows(media(x1), media(x2))
-  observations(x) <- dplyr::bind_rows(observations(x1), observations(x2))
+  x <- x
+  deployments(x) <- dplyr::bind_rows(deployments(x), deployments(y))
+  media(x) <- dplyr::bind_rows(media(x), media(y))
+  observations(x) <- dplyr::bind_rows(observations(x), observations(y))
 
   # Merge/update metadata
   x$name <- NA
   x$id <- NA
   x$created <- format(Sys.time(), "%Y-%m-%dT%H:%M:%SZ")
   x$title <- NA
-  x$contributors <- remove_duplicates(c(x1$contributors, x2$contributors))
-  x$description <- paste(x1$description, x2$description, sep = "/n")
+  x$contributors <- remove_duplicates(c(x$contributors, y$contributors))
+  x$description <- paste(x$description, y$description, sep = "/n")
   x$version <- "1.0"
-  x$keywords <- unique(c(x1$keywords, x2$keywords))
+  x$keywords <- unique(c(x$keywords, y$keywords))
   x$image <- NULL
   x$homepage <- NULL
-  x$sources <- remove_duplicates(c(x1$sources, x2$sources))
-  x$licenses <- remove_duplicates(c(x1$licenses, x2$licenses))
+  x$sources <- remove_duplicates(c(x$sources, y$sources))
+  x$licenses <- remove_duplicates(c(x$licenses, y$licenses))
   x$project <- NULL
-  x$projects <- list(x1$project, x2$project)
+  x$projects <- list(x$project, y$project)
   x$bibliographicCitation <- NULL
   x$coordinatePrecision <-
-    max(x1$coordinatePrecision, x2$coordinatePrecision, na.rm = TRUE)
+    max(x$coordinatePrecision, y$coordinatePrecision, na.rm = TRUE)
 
-  if (!is.null(x1$id)) {
-    relatedIdentifiers_x1 <- list(
+  if (!is.null(x$id)) {
+    relatedIdentifiers_x <- list(
       relationType = "IsDerivedFrom",
-      relatedIdentifier = as.character(x1$id),
+      relatedIdentifier = as.character(x$id),
       resourceTypeGeneral = "Data package",
       relatedIdentifierType = "id"
     )
   } else {
-    relatedIdentifiers_x1 <- list()
+    relatedIdentifiers_x <- list()
   }
-  if (!is.null(x2$id)) {
-    relatedIdentifiers_x2 <- list(
+  if (!is.null(y$id)) {
+    relatedIdentifiers_y <- list(
       relationType = "IsDerivedFrom",
-      relatedIdentifier = as.character(x2$id),
+      relatedIdentifier = as.character(y$id),
       resourceTypeGeneral = "Data package",
       relatedIdentifierType = "id"
     )
   } else {
-    relatedIdentifiers_x2 <- list()
+    relatedIdentifiers_y <- list()
   }
-  new_relatedIdentifiers <- list(relatedIdentifiers_x1, relatedIdentifiers_x2)
+  new_relatedIdentifiers <- list(relatedIdentifiers_x, relatedIdentifiers_y)
   x$relatedIdentifiers <- remove_duplicates(
-    c(x1$relatedIdentifiers, x2$relatedIdentifiers, new_relatedIdentifiers)
+    c(x$relatedIdentifiers, y$relatedIdentifiers, new_relatedIdentifiers)
   )
 
-  x$references <- unique(c(x1$references, x2$references))
+  x$references <- unique(c(x$references, y$references))
 
   x <- x %>%
     update_spatial() %>%
