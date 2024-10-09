1666fa6a2bed1900fa363dae3359c71c8cbdaa35
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-10-09 17:32:55 +0200

    reorder

--- tests/testthat/test-deployments.R
+++ tests/testthat/test-deployments.R
@@ -1,3 +1,24 @@
+test_that("deployments<-() returns error when value is not a data frame", {
+  skip_if_offline()
+  x <- example_dataset()
+  expect_error(
+    deployments(x) <- "not_a_data_frame",
+    class = "camtrapdp_error_assignment_wrong_class"
+  )
+})
+
+test_that("deployments<-() returns error when expected columns are missing", {
+  skip_if_offline()
+  x <- example_dataset()
+  expect_error(
+    deployments(x) <- data.frame(
+      deploymentStart = 1:3,
+      deploymentEnd = 1:3,
+      latitude = 1:3),
+    class = "camtrapdp_error_deployments_columns_missing"
+  )
+})
+
 test_that("deployments() returns a tibble", {
   skip_if_offline()
   x <- example_dataset()
@@ -21,25 +42,4 @@ test_that("deployments<-() assigns a data frame (as tibble) as deployments", {
   deployments(x) <- df
   expect_identical(deployments(x), dplyr::as_tibble(df))
   expect_s3_class(deployments(x), "tbl")
-})
-
-test_that("deployments<-() returns error when value is not a data frame", {
-  skip_if_offline()
-  x <- example_dataset()
-  expect_error(
-    deployments(x) <- "not_a_data_frame",
-    class = "camtrapdp_error_assignment_wrong_class"
-  )
-})
-
-test_that("deployments<-() returns error when expected columns are missing", {
-  skip_if_offline()
-  x <- example_dataset()
-  expect_error(
-    deployments(x) <- data.frame(
-      deploymentStart = 1:3,
-      deploymentEnd = 1:3,
-      latitude = 1:3),
-    class = "camtrapdp_error_deployments_columns_missing"
-  )
 })
--- tests/testthat/test-observations.R
+++ tests/testthat/test-observations.R
@@ -1,3 +1,21 @@
+test_that("observations<-() returns error when value is not a data frame", {
+  skip_if_offline()
+  x <- example_dataset()
+  expect_error(
+    observations(x) <- "not_a_data_frame",
+    class = "camtrapdp_error_assignment_wrong_class"
+  )
+})
+
+test_that("observations<-() returns error when expected columns are missing", {
+  skip_if_offline()
+  x <- example_dataset()
+  expect_error(
+    observations(x) <- data.frame(a = 1:3),
+    class = "camtrapdp_error_observations_columns_missing"
+  )
+})
+
 test_that("observations() returns a tibble", {
   skip_if_offline()
   x <- example_dataset()
@@ -17,22 +35,4 @@ test_that("observations<-() assigns a data frame (as tibble) as observations", {
   observations(x) <- df
   expect_identical(observations(x), dplyr::as_tibble(df))
   expect_s3_class(observations(x), "tbl")
-})
-
-test_that("observations<-() returns error when value is not a data frame", {
-  skip_if_offline()
-  x <- example_dataset()
-  expect_error(
-    observations(x) <- "not_a_data_frame",
-    class = "camtrapdp_error_assignment_wrong_class"
-  )
-})
-
-test_that("observations<-() returns error when expected columns are missing", {
-  skip_if_offline()
-  x <- example_dataset()
-  expect_error(
-    observations(x) <- data.frame(a = 1:3),
-    class = "camtrapdp_error_observations_columns_missing"
-  )
 })
