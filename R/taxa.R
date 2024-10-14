207816058c6c79b18e0ca2e596ffd6f38f9f68da
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-10-14 15:30:46 +0200

    remove duplicates, but keep row with most taxonomical information

--- R/taxa.R
+++ R/taxa.R
@@ -13,10 +13,28 @@
 #' taxa(x)
 taxa <- function(x) {
   check_camtrapdp(x)
-  observations(x) %>%
+  taxa <- observations(x) %>%
     dplyr::filter(!is.na(.data$scientificName)) %>%
     dplyr::select("scientificName", dplyr::starts_with("taxon.")) %>%
     dplyr::distinct() %>%
     dplyr::rename_with(~ sub("^taxon.", "", .x)) %>%
     dplyr::arrange(scientificName)
+
+  # Identify duplicates with the least information, to remove
+  duplicates_to_remove <- taxa %>%
+    dplyr::mutate(count_non_na = rowSums(!is.na(.))) %>%
+    {if ("taxonID" %in% names(taxa)) dplyr::group_by(., scientificName, taxonID)
+      else dplyr::group_by(., scientificName)} %>%
+    dplyr::filter(dplyr::n() > 1) %>%
+    dplyr::arrange(count_non_na) %>%
+    dplyr::slice_head(n = 1) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(-count_non_na)
+
+  # Remove duplicates
+  unique_taxa <-
+    taxa %>%
+    dplyr::anti_join(duplicates_to_remove, by = names(duplicates_to_remove))
+
+  return(unique_taxa)
 }
