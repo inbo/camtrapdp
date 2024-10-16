e166bb6d85dcc0a4640eebad77d4072e712ad5c3
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-10-16 18:22:16 +0200

    add visible binding for global variables

--- R/taxa.R
+++ R/taxa.R
@@ -23,7 +23,7 @@ taxa <- function(x) {
     dplyr::select("scientificName", dplyr::starts_with("taxon.")) %>%
     dplyr::distinct() %>%
     dplyr::rename_with(~ sub("^taxon.", "", .x)) %>%
-    dplyr::arrange(scientificName)
+    dplyr::arrange(.data$scientificName)
 
   # Remove duplicates without taxonID
   if ("taxonID" %in% names(taxa)) {
@@ -42,13 +42,13 @@ taxa <- function(x) {
   # Remove duplicates with the least information
   duplicates_with_least_info <-
     taxa %>%
-    dplyr::mutate(columns_with_info = rowSums(!is.na(.))) %>%
+    dplyr::mutate(.data$columns_with_info = rowSums(!is.na(.))) %>%
     dplyr::group_by(.data$scientificName) %>%
     dplyr::filter(dplyr::n() > 1) %>%
-    dplyr::arrange(dplyr::desc(columns_with_info)) %>%
+    dplyr::arrange(dplyr::desc(.data$columns_with_info)) %>%
     dplyr::slice_tail(n = -1) %>% # Remove first row from group (with most info)
     dplyr::ungroup() %>%
-    dplyr::select(-columns_with_info)
+    dplyr::select(-.data$columns_with_info)
   taxa <- dplyr::anti_join(
     taxa,
     duplicates_with_least_info,
