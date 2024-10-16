acd0b2b250fb3ca84c99e07073583a5a1be73963
Peter Desmet <peter.desmet@inbo.be>
2024-10-16 09:37:28 +0200

    Reorder tests

--- tests/testthat/test-deployments.R
+++ tests/testthat/test-deployments.R
@@ -1,12 +1,3 @@
-test_that("deployments<-() returns error when value is not a data frame", {
-  skip_if_offline()
-  x <- example_dataset()
-  expect_error(
-    deployments(x) <- "not_a_data_frame",
-    class = "camtrapdp_error_assignment_wrong_class"
-  )
-})
-
 test_that("deployments() returns a tibble", {
   skip_if_offline()
   x <- example_dataset()
@@ -19,7 +10,7 @@ test_that("deployments() returns the deployments", {
   expect_identical(deployments(x), x$data$deployments)
 })
 
-test_that("deployments<-() assigns a data frame (as tibble) as deployments", {
+test_that("deployments()<- assigns a data frame (as tibble) as deployments", {
   skip_if_offline()
   x <- example_dataset()
   df <- data.frame(
@@ -32,8 +23,16 @@ test_that("deployments<-() assigns a data frame (as tibble) as deployments", {
   expect_s3_class(deployments(x), "tbl")
 })
 
+test_that("deployments()<- returns error when value is not a data frame", {
+  skip_if_offline()
+  x <- example_dataset()
+  expect_error(
+    deployments(x) <- "not_a_data_frame",
+    class = "camtrapdp_error_assignment_wrong_class"
+  )
+})
 
-test_that("deployments<-() updates temporal and spatial scope in metadata", {
+test_that("deployments<-() updates spatial and temporal scope in metadata", {
   skip_if_offline()
   x <- example_dataset()
   deployments(x) <- data.frame(
@@ -43,6 +42,7 @@ test_that("deployments<-() updates temporal and spatial scope in metadata", {
     deploymentStart = as.POSIXct("2020-05-30 02:57:37 UTC", tz = "UTC"),
     deploymentEnd = as.POSIXct("2020-07-01 09:41:41 UTC", tz = "UTC")
   )
+
   expected_spatial <- list(
     type = "Polygon",
     coordinates = array(
@@ -53,7 +53,8 @@ test_that("deployments<-() updates temporal and spatial scope in metadata", {
       dim = c(1, 5, 2)
     )
   )
-  expect_equal(x$spatial, expected_spatial)
+  expect_identical(x$spatial, expected_spatial)
+
   expect_identical(x$temporal$start, "2020-05-30")
   expect_identical(x$temporal$end, "2020-07-01")
 })
--- tests/testthat/test-observations.R
+++ tests/testthat/test-observations.R
@@ -1,12 +1,3 @@
-test_that("observations<-() returns error when value is not a data frame", {
-  skip_if_offline()
-  x <- example_dataset()
-  expect_error(
-    observations(x) <- "not_a_data_frame",
-    class = "camtrapdp_error_assignment_wrong_class"
-  )
-})
-
 test_that("observations() returns a tibble", {
   skip_if_offline()
   x <- example_dataset()
@@ -19,13 +10,22 @@ test_that("observations() returns the observations", {
   expect_identical(observations(x), x$data$observations)
 })
 
-test_that("observations<-() assigns a data frame (as tibble) as observations", {
+test_that("observations()<- assigns a data frame (as tibble) as observations", {
   skip_if_offline()
   x <- example_dataset()
   df <- data.frame(scientificName = 1:3)
   observations(x) <- df
   expect_identical(observations(x), dplyr::as_tibble(df))
   expect_s3_class(observations(x), "tbl")
+})
+
+test_that("observations()<- returns error when value is not a data frame", {
+  skip_if_offline()
+  x <- example_dataset()
+  expect_error(
+    observations(x) <- "not_a_data_frame",
+    class = "camtrapdp_error_assignment_wrong_class"
+  )
 })
 
 test_that("observations<-() updates taxonomic scope in metadata", {
@@ -42,8 +42,8 @@ test_that("observations<-() updates taxonomic scope in metadata", {
       scientificName = "Anas platyrhynchos",
       taxonID = "https://www.checklistbank.org/dataset/COL2023/taxon/DGP6",
       taxonRank = "species"
-      )
     )
+  )
   expect_identical(x$taxonomic, expected_taxonomic)
 })
 
