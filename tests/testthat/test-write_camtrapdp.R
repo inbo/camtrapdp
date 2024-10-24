e26a7d38a1024a87a6dd2912737039a8af760043
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-10-24 12:29:09 +0200

    Update test-write_camtrapdp.R

--- tests/testthat/test-write_camtrapdp.R
+++ tests/testthat/test-write_camtrapdp.R
@@ -15,3 +15,53 @@ test_that("write_camtrapdp() returns output Camera Trap Data Package
 
   expect_identical(x_written, x_from_file)
 })
+
+test_that("write_camtrapdp() writes the same json and CSV files as the original
+          files", {
+  x <- example_dataset()
+  temp_dir <- file.path(tempdir(), "package")
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  write_camtrapdp(x, temp_dir)
+
+  # datapackage.json
+  original_datapackage <- jsonlite::fromJSON(
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0.1/example/datapackage.json",
+    simplifyDataFrame = FALSE, simplifyVector = TRUE
+    )
+  written_datapackage <- jsonlite::fromJSON(
+    file.path(temp_dir, "datapackage.json"),
+    simplifyDataFrame = FALSE, simplifyVector = TRUE
+    )
+  expect_identical(original_datapackage, written_datapackage)
+
+  # deployments.csv
+  original_deployments <- suppressMessages(readr::read_delim(
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/deployments.csv",
+    delim = ","
+  ))
+  written_deployments <- suppressMessages(readr::read_delim(
+    file.path(temp_dir, "deployments.csv")
+  ))
+  expect_identical(original_deployments, written_deployments)
+
+  # media.csv
+  original_media <- suppressMessages(readr::read_delim(
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/media.csv",
+    delim = ","
+  ))
+  written_media <- suppressMessages(readr::read_delim(
+    file.path(temp_dir, "media.csv")
+  ))
+  expect_identical(original_media, written_media)
+
+  # observations.csv
+  original_observations <- suppressMessages(readr::read_delim(
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/observations.csv",
+    delim = ","
+  ))
+  written_observations <- suppressMessages(readr::read_delim(
+    file.path(temp_dir, "observations.csv")
+  ))
+  expect_identical(original_observations, written_observations)
+
+})
