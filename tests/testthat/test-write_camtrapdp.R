03b9ff65ab797aa0a153b51714f6b0c19b3c6d71
Peter Desmet <peter.desmet@inbo.be>
2024-10-25 14:07:47 +0200

    Add skip_if_offline() + suggest readr

--- tests/testthat/test-write_camtrapdp.R
+++ tests/testthat/test-write_camtrapdp.R
@@ -14,8 +14,8 @@ test_that("write_camtrapdp() writes datapackage.json and CSV files to a
   expect_invisible(write_camtrapdp(x, temp_dir))
 })
 
-test_that("write_camtrapdp() writes the same json and CSV files as the original
-          files", {
+test_that("write_camtrapdp() writes the unaltered example dataset as is", {
+  skip_if_offline()
   x <- example_dataset()
   temp_dir <- file.path(tempdir(), "package")
   on.exit(unlink(temp_dir, recursive = TRUE))
@@ -30,35 +30,35 @@ test_that("write_camtrapdp() writes the same json and CSV files as the original
     file.path(temp_dir, "datapackage.json"),
     simplifyDataFrame = FALSE, simplifyVector = TRUE
     )
-  expect_identical(original_datapackage, written_datapackage)
+  expect_identical(written_datapackage, original_datapackage)
 
   # deployments.csv
   original_deployments <- suppressMessages(readr::read_delim(
-    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/deployments.csv",
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0.1/example/deployments.csv",
     delim = ","
   ))
   written_deployments <- suppressMessages(readr::read_delim(
     file.path(temp_dir, "deployments.csv")
   ))
-  expect_identical(original_deployments, written_deployments)
+  expect_identical(written_deployments, original_deployments)
 
   # media.csv
   original_media <- suppressMessages(readr::read_delim(
-    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/media.csv",
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0.1/example/media.csv",
     delim = ","
   ))
   written_media <- suppressMessages(readr::read_delim(
     file.path(temp_dir, "media.csv")
   ))
-  expect_identical(original_media, written_media)
+  expect_identical(written_media, original_media)
 
   # observations.csv
   original_observations <- suppressMessages(readr::read_delim(
-    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0/example/observations.csv",
+    "https://raw.githubusercontent.com/tdwg/camtrap-dp/1.0.1/example/observations.csv",
     delim = ","
   ))
   written_observations <- suppressMessages(readr::read_delim(
     file.path(temp_dir, "observations.csv")
   ))
-  expect_identical(original_observations, written_observations)
+  expect_identical(written_observations, original_observations)
 })
