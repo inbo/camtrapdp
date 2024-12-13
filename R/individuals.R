965f72760eaa64f58144402db03d07b4e80b0c84
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-12-13 10:45:21 +0100

    Create individuals.R

---
+++ R/individuals.R
@@ -0,0 +1,28 @@
+#' Get locations
+#'
+#' Gets the (unique) individuals from the observations of a Camera Trap Data
+#' Package object.
+#'
+#' @inheritParams print.camtrapdp
+#' @return A [tibble::tibble()] data frame with the individuals that have an
+#' `individualID`, containing the following columns:
+#'   - `individualID`
+#'   - `scientificName`
+#'   - `lifeStage`
+#'   - `sex`
+#' @family accessor functions
+#' @export
+#' @examples
+#' x <- example_dataset()
+#' individuals(x)
+individuals <- function(x) {
+  check_camtrapdp(x)
+  observations(x) %>%
+    dplyr::distinct(
+      .data$individualID,
+      .data$scientificName,
+      .data$lifeStage,
+      .data$sex,
+    ) %>%
+    dplyr::filter(!is.na(individualID))
+}
