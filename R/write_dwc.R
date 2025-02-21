a5f293e4c8a2db00f222d9114232fa5d9a6b845e
Peter Desmet <peter.desmet@inbo.be>
2025-02-21 14:37:04 +0100

    Switch order

--- R/write_dwc.R
+++ R/write_dwc.R
@@ -148,8 +148,8 @@ write_dwc <- function(x, directory) {
         dplyr::case_when(
           .data$baitUse == TRUE ~ "camera trap with bait",
           .data$baitUse == FALSE ~ "camera trap without bait",
-          is.na(.data$baitUse) & is.na(.data$featureType) ~ "",
-          is.na(.data$baitUse) & !is.na(.data$featureType) ~ "camera trap"
+          is.na(.data$baitUse) & !is.na(.data$featureType) ~ "camera trap",
+          is.na(.data$baitUse) & is.na(.data$featureType) ~ ""
           ),
         dplyr::if_else(
           is.na(.data$featureType),
