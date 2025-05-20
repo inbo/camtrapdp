06d59a958ed6ebaba53514fb0a87fca3bda487ca
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-05-20 17:40:54 +0200

    only extract contributors

--- R/contributors.R
+++ R/contributors.R
@@ -11,35 +11,10 @@
 #' x <- example_dataset()
 #' contributors(x)
 contributors <- function(x) {
-  orcid_regex <- "(\\d{4}-){3}\\d{3}(\\d|X)"
   contributors <-
-    purrr::map_dfr(
-      x$contributors,
-      ~ as.data.frame(., stringsAsFactors = FALSE)
-    ) %>%
-    # dplyr::filter(!.data$role %in% c("rightsHolder", "publisher")) %>%
-    mutate_when_missing(path = character()) %>% # Guarantee path col
-    dplyr::mutate(
-      first_name = purrr::map_chr(
-        .data$title,
-        ~ strsplit(.x, " ", fixed = TRUE)[[1]][1] # First string before space
-      ),
-      last_name = purrr::map_chr(
-        .data$title,
-        ~ sub("^\\S* ", "", .x) # Remove string up until first space
-      ),
-      orcid = ifelse( # Move ORCID from path to separate column
-        !is.na(regexpr(orcid_regex, .data$path)),
-        regmatches(.data$path, regexpr(orcid_regex, .data$path)),
-        NA_character_
-      ),
-      path = ifelse(
-        grepl(orcid_regex, .data$path),
-        NA_character_,
-        .data$path
-      )
-    ) %>%
+    x$contributors %>%
+    purrr::map(., ~ as.data.frame(.)) %>%
+    purrr::list_rbind() %>%
     dplyr::as_tibble()
-
   return(contributors)
 }
